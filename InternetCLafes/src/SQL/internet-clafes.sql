f76893371cde5d74345c436ee1be0b89557f8e46
Matthew Avrillio <matthew.avrillio@binus.ac.id>
2023-12-04 19:52:29 +0700

    Add SQL File

---
+++ InternetCLafes/src/SQL/internet-clafes.sql
@@ -0,0 +1,248 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2023 at 01:39 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `internet-clafes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job`
+--
+
+CREATE TABLE `job` (
+  `JobID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `JobStatus` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pc`
+--
+
+CREATE TABLE `pc` (
+  `PC_ID` int(11) NOT NULL,
+  `PC_Condition` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pcbook`
+--
+
+CREATE TABLE `pcbook` (
+  `BookID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `BookedDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report`
+--
+
+CREATE TABLE `report` (
+  `Report_ID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `UserRole` varchar(255) NOT NULL,
+  `ReportDate` date NOT NULL,
+  `ReportNote` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction detail`
+--
+
+CREATE TABLE `transaction detail` (
+  `TransactionID` int(11) NOT NULL,
+  `PC_ID` int(11) NOT NULL,
+  `CustomerName` varchar(255) NOT NULL,
+  `BookedTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactionheader`
+--
+
+CREATE TABLE `transactionheader` (
+  `TransactionID` int(11) NOT NULL,
+  `StaffID` int(11) NOT NULL,
+  `StaffName` varchar(255) NOT NULL,
+  `TransactionDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
+  `UserPassword` varchar(255) NOT NULL,
+  `UserAge` int(11) NOT NULL,
+  `UserRole` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `job`
+--
+ALTER TABLE `job`
+  ADD PRIMARY KEY (`JobID`),
+  ADD KEY `PC_ID` (`PC_ID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `pc`
+--
+ALTER TABLE `pc`
+  ADD PRIMARY KEY (`PC_ID`);
+
+--
+-- Indexes for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  ADD PRIMARY KEY (`BookID`),
+  ADD KEY `PC_ID` (`PC_ID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Indexes for table `report`
+--
+ALTER TABLE `report`
+  ADD PRIMARY KEY (`Report_ID`),
+  ADD KEY `PC_ID` (`PC_ID`);
+
+--
+-- Indexes for table `transaction detail`
+--
+ALTER TABLE `transaction detail`
+  ADD PRIMARY KEY (`PC_ID`,`TransactionID`),
+  ADD KEY `TransactionID` (`TransactionID`);
+
+--
+-- Indexes for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  ADD PRIMARY KEY (`TransactionID`),
+  ADD KEY `StaffID` (`StaffID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `job`
+--
+ALTER TABLE `job`
+  MODIFY `JobID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pc`
+--
+ALTER TABLE `pc`
+  MODIFY `PC_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  MODIFY `BookID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report`
+--
+ALTER TABLE `report`
+  MODIFY `Report_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  MODIFY `TransactionID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `job`
+--
+ALTER TABLE `job`
+  ADD CONSTRAINT `job_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`),
+  ADD CONSTRAINT `job_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `pcbook`
+--
+ALTER TABLE `pcbook`
+  ADD CONSTRAINT `pcbook_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`),
+  ADD CONSTRAINT `pcbook_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`);
+
+--
+-- Constraints for table `transaction detail`
+--
+ALTER TABLE `transaction detail`
+  ADD CONSTRAINT `transaction detail_ibfk_1` FOREIGN KEY (`TransactionID`) REFERENCES `transactionheader` (`TransactionID`),
+  ADD CONSTRAINT `transaction detail_ibfk_2` FOREIGN KEY (`PC_ID`) REFERENCES `pc` (`PC_ID`);
+
+--
+-- Constraints for table `transactionheader`
+--
+ALTER TABLE `transactionheader`
+  ADD CONSTRAINT `transactionheader_ibfk_1` FOREIGN KEY (`StaffID`) REFERENCES `user` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
